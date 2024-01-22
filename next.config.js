4db1724d7ac4c592e65d009f337eabe4549ae7ae
Animesh garai <animeshgarai09@gmail.com>
2024-01-22 08:45:05 +0530

    config change

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        serverActions: true,
+        serverComponentsExternalPackages: ["mongoose"],
+    },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
+}
 
 module.exports = nextConfig
